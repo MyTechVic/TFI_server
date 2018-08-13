ec66f40f506b18cffb73570ed012086191dcef06
Matt <deal.sy@hotmail.com>
2018-08-13 13:48:23 +1000

    added port change

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
     development: {
         //mongodb connection settings
         database: {
-            host:   'mongodb://<Matt>:<Outbreak23>@ds119652.mlab.com:19652/tfi-database',
+            host: 'mongodb://<Matt>:<Outbreak23>@ds119652.mlab.com:19652/tfi-database',
         }
-    };
+    }
 }
 module.exports = config;

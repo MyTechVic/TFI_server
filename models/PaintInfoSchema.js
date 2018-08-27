62049f1e52e9a9768344adafdb0562ce22bbf178
Matt <deal.sy@hotmail.com>
2018-08-27 13:35:09 +1000

    updated Schema

--- models/PaintInfoSchema.js
+++ models/PaintInfoSchema.js
@@ -4,19 +4,35 @@ const Schema = mongoose.Schema;
 
   // Create schema
  const PaintInfoSchema =  new Schema({
+        customerID: {
+            required: true,
+            index: true,
+            unique: true,
+            type: String
+        },
     	companyName: {
+            required: true,
+            index: true,
     		type: String
     	},
         curtainCodes: {
+            required: true,
+            index: true,
         	type: String
         },
         sinageCodes: {
+            required: true,
+            index: true,
         	type: String
         },
         Notes: {
+            required: true,
+            index: true,
         	type: String
         },
         Method: {
+            required: true,
+            index: true,
         	type: String
         },
     },{
